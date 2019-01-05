0e16941030c5c80019e998dd43766ea0219c3fbd
John Mark Redding <johnmarkredding@gmail.com>
2019-01-05 02:47:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,14 @@ function scuberGreetingForFeet (ride) {
   }
   return result
 }
+function terneryGreetingForFeet (ride) {
+  let result;
+  if (ride <= 400){
+    result = "This one is on me!";
+  } else if ((ride > 2000) && (ride <= 2500)) {
+    result = "I will gladly take your thirty bucks."
+  } else {
+    result = "No can do."
+  }
+  return result
+}
