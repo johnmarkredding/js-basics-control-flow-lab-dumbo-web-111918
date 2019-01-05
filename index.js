e5d5e036540cfe8bfc956abb00f2450e7304a5a9
John Mark Redding <johnmarkredding@gmail.com>
2019-01-05 02:54:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function switchOnCharmFromTip(tip) {
     case 'generous': 
       result = "Thank you so much.";
       break;
-    case 'not generous':
+    case 'not as generous':
       result = "Thank you.";
       break;
     default: 
